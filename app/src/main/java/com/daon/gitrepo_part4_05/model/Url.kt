c2bf7a029cdb5d62dcbc5dd8a222fb9dd9c294d7
daon1213 <diaeiyo1213@naver.com>
2022-04-27 16:04:02 +0900

    url commit!

---
+++ app/src/main/java/com/daon/gitrepo_part4_05/model/Url.kt
@@ -0,0 +1,8 @@
+package com.daon.gitrepo_part4_05.model
+
+object Url {
+    const val GITHUB_BASE_URL = "https://github.com"
+    const val GITHUB_API_URL = "https://api.github.com"
+    const val GITHUB_CLIENT_SECRET="354ed8e64955d8abd3919532199b48dcd43a2bc1"
+    const val GITHUB_CLIENT_ID="Iv1.a50a3ab241111e18"
+}
